772fb753ebdb51191369e332501eb4e74cb8c08f
Christian Hellberg <christian.hellberg83@gmail.com>
2026-03-07 19:30:13 +0100

    delade

--- js/main.js
+++ js/main.js
@@ -1,4 +1,54 @@
 // Main js-file
+class Project {
+    #title;
+    #createdDate;
+    #participants;
+
+    constructor(title){
+        this.#title = title;
+        this.#createdDate = Date();
+        this.#participants = [];
+    }
+
+    addParticipant(participant){
+        this.#participants.push(participant);
+    }
+    showParticipants(){
+        return this.#participants;
+    }
+
+    showTitle(){
+        return this.#title;
+    }
+    changeTitle(newTitle){
+        this.#title = newTitle;
+    }
+    showDateOfCreation(){
+        return this.#createdDate;
+    }
+}
+class ProjectParticipant {
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    }
+
+
+
+const myProject = new Project("Mitt första projekt");
+console.log(myProject.showTitle());
+myProject.changeTitle("Mitt ändrade projekt");
+console.log(myProject.showTitle());
+console.log(myProject.showDateOfCreation());
+    const user = new ProjectParticipant("Christian","Hellberg");
+    const user2 = new ProjectParticipant("Anders","Andersson");
+    myProject.addParticipant(user);
+    myProject.addParticipant(user2);
+    console.log(myProject.showParticipants().forEach(participant => {
+        console.log(`${participant.firstName} ${participant.lastName}`);
+    }));
+
 /*import {ProjectParticipant} from "./ProjectParticipant.js";
 
 const person1 = new ProjectParticipant("Christian","Hellberg");
@@ -15,7 +65,7 @@ const projectPatricipants = [person1, person2];
 projectPatricipants.push(person3); 
 
 console.log(JSON.stringify(projectPatricipants))
-*/
+
 const projects = [];
 const addProject  = (project => {
     projects.push(project);
@@ -36,7 +86,7 @@ document.querySelector("#amp-reg-form").addEventListener("submit", event => {
     } catch (error) {
         console.error(error);
     }
-            console.log(projects);
+            //console.log(projects);
 });
 
 const projectList = document.querySelector("#project-list");
@@ -44,5 +94,6 @@ const renderProjects = () => {
     projects.forEach(project => {
             projectList.insertAdjacentHTML(`beforeend`, `<li>${project.title}</li>`);
     })
-    
+   
 }
+ */
